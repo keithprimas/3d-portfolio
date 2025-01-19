27a10fe063f62bb21eff2b7e8f54efd9434b9eda
Keith Primas <keithprimas@gmail.com>
2025-01-19 11:37:36 -0500

    about section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -248,7 +248,7 @@ export const navLinks = [
   
   export const calculateSizes = (isSmall, isMobile, isTablet) => {
     return {
-      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskScale: isSmall ? 0.05 : isMobile ? 0.053 : 0.056,
       deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
       cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
       reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Globe from 'react-globe.gl'
 import Button from '../components/Button'
+import { useState } from 'react'
 
 const About = () => {
+    const [hasCopied, setHasCopied] = useState(false)
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('keithprimas@gmail.com');
+        setHasCopied(true);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    }
+
   return (
     <section className='c-space my-20'>
         <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
@@ -54,6 +65,34 @@ const About = () => {
                           I'm based in Charlotte, North Carolina, USA
                         </p>
                         <Button name='Contact Me' isBeam containerClass='w-full mt-10'/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='xl:col-span-2 xl:row-span-3'>
+                <div className='grid-container'>
+                        <img src='/assets/grid3.png' alt='grid-3' className='w-full sm:h-[266px] h-fit object-contain'/>
+                        <div>
+                             <p className='grid-headtext'>My journey</p>
+                             <p className='grid-subtext'>After being a pharmacy tech for more than 5 years, I decided I needed a career change. 
+                                From there, I started learning web development in 2023 and attended a fullstack bootcamp which I graudated from in 2024.
+                                Since then, I have been working on various projects to hone my skills and build my portfolio to pursure a career in web development.
+                             </p>
+                        </div>
+                </div>
+            </div>
+
+            <div className='xl:col-span-1 xl:row-span-2'>
+                <div className='grid-container'>
+                    <img src='/assets/grid4.png' alt='grid-4' className='w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top'/>
+                    <div className='space-y-2'>
+                        <p className='grid-subtext text-center'>Email Me</p>
+                        <div className='copy-container' onClick={handleCopy}>
+                            <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt='copy'/>
+                            <p className='lg:text-2xl md:text-xl font-medium text-gray_gradient text-white'>
+                                keithprimas@gmail.com
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
