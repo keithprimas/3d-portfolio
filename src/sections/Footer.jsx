6ddc2dd60efc6eb16ebf4d5b2235b329ecf68d2e
Keith Primas <keithprimas@gmail.com>
2025-01-21 20:39:09 -0500

    removed footer border

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='c-space pt-7 pb-3 border-t border-black-300 flex items-center justify-center flex-wrap gap-5'>
+    <section className='c-space pt-7 pb-3 flex items-center justify-center flex-wrap gap-5'>
         <div className='text-white-500 flex gap-2'>
             <div className='flex gap-3'>
                 <div className='social-icon'>
